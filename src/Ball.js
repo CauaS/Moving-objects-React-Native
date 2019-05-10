110f7f472eedae69e4b5ab1ba4b37148d539014b
CauaS <37887533+CauaS@users.noreply.github.com>
2019-05-09 22:59:55 -0300

    Add files via upload

---
+++ src/Ball.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
+
+export default class Ball extends React.Component {
+
+  componentWillMount(){
+    this.position = new Animated.ValueXY(0,0);
+    Animated.spring(this.position,  {toValue: { x:200, y:500 }}).start();
+  }
+
+  render() {
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  ball: {
+    height: 50,
+    width: 50, 
+    backgroundColor: 'black'
+  }
+});
