09f22765193b350fbce48555725824c776bba2b3
CauaS <37887533+CauaS@users.noreply.github.com>
2019-05-11 19:52:28 -0300

    Add files via upload

--- src/Deck.js
+++ src/Deck.js
@@ -10,9 +10,21 @@ class Deck extends Component {
     constructor(props){
         super(props);
 
-        const panResponder = PanResponder.create({})
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            //this function is executed any time a user tas on the screem.
+            onStartShouldSetPanResponder: (event, gesture) => true,
 
-        this.state = { panResponder };
+            //when the user starts to drag his finger around the sreem.
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y:gesture.dy });
+            },
+
+            //when the user remove his finger on the screem.
+            onPanResponderRelease: () => {}
+        })
+
+        this.state={ panResponder, position };
     }
 
     rendersCards(){
@@ -23,9 +35,12 @@ class Deck extends Component {
 
     render(){
         return( 
-            <View>
+            <Animated.View 
+                {...this.state.panResponder.panHandlers}
+                style={this.state.position.getLayout()}
+            >
                 {this.rendersCards()}
-            </View>
+            </Animated.View>
         );
     }
 }
